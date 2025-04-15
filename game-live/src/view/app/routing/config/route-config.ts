94e78a981508ea20b8d6b41072c5b923e5fb686e
Nikita <nikitalomieiko@gmail.com>
2025-04-15 17:02:41 +0300

    fix

--- game-live/src/view/app/routing/config/route-config.ts
+++ game-live/src/view/app/routing/config/route-config.ts
@@ -3,11 +3,13 @@ import { HomePage } from "view/pages/home";
 import { GamePage } from "view/pages/game";
 
 enum enumRouting {
+  BASE = "BASE",
   HOME = "HOME",
   GAME = "GAME",
 }
 
 export const Routes: Record<enumRouting, RouteRecordRaw> = {
+  [enumRouting.BASE]: { name: "/", path: "/", component: HomePage },
   [enumRouting.HOME]: { name: "/home", path: "/home", component: HomePage },
   [enumRouting.GAME]: { name: "/game", path: "/game", component: GamePage },
 };
